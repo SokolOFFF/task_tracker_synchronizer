dc61db74daf59fce090bd760812f79cd4b4d03e1
SokolOFFF <egor1sokolov@gmail.com>
2024-04-25 02:08:46 +0300

    api part of backend implemented

--- src/api_functions.py
+++ src/api_functions.py
@@ -1,10 +1,12 @@
+import datetime
+
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 
 
-# Gets jira issue data
-def get_jira_issue_data(issue_key):
+# Gets jira issue data from API
+def __get_jira_issue_data(issue_key):
     config = json.load(open('config/config.json', 'r'))
     secrets = json.load(open('config/secrets.json', 'r'))
 
@@ -24,11 +26,25 @@ def get_jira_issue_data(issue_key):
         auth=auth
     )
 
-    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+    return json.loads(response.text)
 
 
-# Gets youtrack data
-def get_youtrack_issue_data(issue_key):
+# Returns json with needed fields from the whole API data
+def get_jira_issue_json(issue_key):
+    whole_data = __get_jira_issue_data(issue_key)
+    fields = whole_data['fields']
+    summary = fields['summary']
+    description = fields['description']['content'][0]['content'][0]['text']
+    status = fields['status']['name']
+    priority = fields['priority']['name']
+    estimation = fields['customfield_10016']
+    due_date = fields['duedate']
+
+    return {"Summary": summary, 'Description': description, "Status": status, "Priority": priority, "Estimation": estimation, "Due date": due_date}
+
+
+# Gets youtrack data from API
+def __get_youtrack_issue_data(issue_key):
     config = json.load(open('config/config.json', 'r'))
     secrets = json.load(open('config/secrets.json', 'r'))
 
@@ -38,7 +54,7 @@ def get_youtrack_issue_data(issue_key):
         "Authorization": token,
         "Accept": "application/json",
     }
-    url = config['youtrack_get_issue_api'] + str(issue_key)
+    url = config['youtrack_get_issue_api'] + str(issue_key) + "?fields=$type,id,summary,description,customFields($type,id,name,value($type,name))"
 
     response = requests.request(
         "GET",
@@ -46,6 +62,108 @@ def get_youtrack_issue_data(issue_key):
         headers=headers
     )
 
-    # print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+    return json.loads(response.text)
 
+
+# Returns json with needed fields from the whole API data
+def get_youtrack_issue_json(issue_key):
+    whole_data = __get_youtrack_issue_data(issue_key)
+    summary = whole_data['summary']
+    description = whole_data['description']
+    status = ''
+    priority = ''
+    estimation = ''
+    due_date = ''
+
+    for field in whole_data['customFields']:
+        if field['name'] == 'State':
+            status = field['value']['name']
+
+        if field['name'] == 'Priority':
+            priority = field['value']['name']
+
+        if field['name'] == 'Estimation':
+            estimation = field['value']
+
+        if field['name'] == 'Due Date':
+            due_date = datetime.datetime.fromtimestamp(field['value'] / 1000).strftime("%Y-%m-%d")
+
+    return {"Summary": summary, 'Description': description, "Status": status, "Priority": priority, "Estimation": estimation, "Due date": due_date}
+
+
+# Function which edits jira issue
+def edit_jira_issue(issue_key, new_json_data):
+    config = json.load(open('config/config.json', 'r'))
+    secrets = json.load(open('config/secrets.json', 'r'))
+
+    url = config['jira_get_issue_api'] + str(issue_key)
+    token = secrets['jira_auth_token']
+    email = secrets['jira_auth_email']
+    auth = HTTPBasicAuth(email, token)
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    payload = json.dumps(
+    {
+        "fields": {
+            "summary": new_json_data['Summary'],
+            "customfield_10016": new_json_data['Estimation'],
+            "duedate": new_json_data['Due date'],
+            "priority": {
+                "name": new_json_data['Priority']
+            },
+            "description": {
+                            "type": "doc",
+                            "version": 1,
+                            "content": [
+                                {
+                                    "type": "paragraph",
+                                    "content": [
+                                        {
+                                            "text": new_json_data['Description'],
+                                            "type": "text"
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+          }
+    })
+
+    response = requests.request(
+        "PUT",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+    )
+
+    url = config['jira_get_issue_api'] + str(issue_key) + "/transitions"
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        auth=auth
+    )
+    transitions_info = json.loads(response.text)
+    transition_id = 0
+    for transition in transitions_info['transitions']:
+        if transition['name'] == new_json_data['Status']:
+            transition_id = transition['id']
+
+    payload = json.dumps(
+    {
+        "transition": {"id": transition_id}
+    })
+    response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+    )
+
+    return response.status_code
--- src/synchronizer.py
+++ src/synchronizer.py
@@ -1,6 +1,6 @@
 from time import sleep
 from datetime import datetime, timedelta
-from api_functions import get_jira_issue_data, get_youtrack_issue_data
+from api_functions import get_jira_issue_json, get_youtrack_issue_json, edit_jira_issue
 import json
 
 config = None
@@ -13,10 +13,35 @@ def update_rules():
     rules = json.load(open('config/rules.json', 'r'))
 
 
+# Applies rule to the data
+def apply_rule(task_1_current_data, task_2_current_data, rule):
+    task_2_new_data = task_2_current_data.copy()
+    for field in rule['fields']:
+        if rule['fields'][field]['rule_type'] == 1:
+            task_2_new_data[field] = task_1_current_data[field]
+        else:
+            relations = rule['fields'][field]['relations']
+            for relation in relations:
+                if relation == task_1_current_data[field]:
+                    task_2_new_data[field] = relations[relation]
+    return task_2_new_data
+
+
+# Checks if two tasks are synchronized
+def check_two_tasks_synchronization(rule):
+    print(f'Checking synchronization of task {rule["task_id_1"]} and {rule["task_id_2"]}')
+    task_2_current_data = get_jira_issue_json(rule["task_id_2"])
+    task_1_current_data = get_youtrack_issue_json(rule["task_id_1"])
+    new_data = apply_rule(task_1_current_data, task_2_current_data, rule)
+    print(f'Updating {rule["task_id_2"]}...')
+    edit_jira_issue(rule["task_id_2"], new_data)
+
+
 # Checks if task trackers are synchronized
-def check_synchronization():
-    print('Checking...')
-    pass
+def check_synchronizations():
+    print('Checking all synchronizations...')
+    for rule in rules:
+        check_two_tasks_synchronization(rules[rule])
 
 
 # Main function
@@ -26,14 +51,15 @@ def main():
     frequency = config['frequency']
     max_difference = timedelta(seconds=int(frequency))
 
-    check_synchronization()
+    update_rules()
+    check_synchronizations()
     last_time_update = datetime.now()
     while True:
         if datetime.now() - last_time_update > max_difference:
             update_rules()
-            check_synchronization()
+            check_synchronizations()
             last_time_update = datetime.now()
-        sleep(1)
+        sleep(5)
 
 
 if __name__ == '__main__':
