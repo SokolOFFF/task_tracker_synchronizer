67a79c084cd07e9223837419ee78d08a733fd880
xdkomel <xdghost.kamil@gmail.com>
2024-04-28 02:34:00 +0300

    finish add rules

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:collection';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart' hide State;
 import 'package:frontend/api.dart';
 import 'package:frontend/models/rule.dart';
 import 'package:frontend/models/tasks_sync.dart';
@@ -92,6 +96,7 @@ class _AllRulesScreenState extends State<AllRulesScreen> {
                 SomeData(:final data) => AllRulesList(
                     rules: data,
                     selected: _selectedRule,
+                    update: _loadRules,
                     onTap: (i) => setState(() => _selectedRule = i),
                     addRule: () => showModalBottomSheet(
                       context: context,
@@ -136,6 +141,7 @@ class AllRulesList extends StatelessWidget {
   final List<TasksSync> rules;
   final void Function(int i) onTap;
   final void Function() addRule;
+  final void Function() update;
   final int? selected;
   const AllRulesList({
     super.key,
@@ -143,6 +149,7 @@ class AllRulesList extends StatelessWidget {
     required this.onTap,
     required this.addRule,
     required this.selected,
+    required this.update,
   });
 
   @override
@@ -152,6 +159,8 @@ class AllRulesList extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          IconButton(onPressed: update, icon: const Icon(Icons.refresh)),
+          const SizedBox(height: 16),
           if (rules.isEmpty)
             Text(
               'No rules set',
@@ -271,15 +280,20 @@ class _SelectTasksScreenState extends State<SelectTasksScreen> {
         (error) => setState(() => _messageError = error),
         (exists) => !exists
             ? setState(() => _messageError = 'Couldn\'t find a Jira issue')
-            : widget.api
-                .checkYoutrackTaskId(_youtrackController.text.trim())
-                .match(
-                  (error) => setState(() => _messageError = error),
-                  (exists) => !exists
-                      ? setState(() => _messageError = 'Couldn\'t find a YouTrack issue')
-                      : Navigator.pushNamed(context, '/addRules'),
-                )
-                .run(),
+            : widget.api.checkYoutrackTaskId(_youtrackController.text.trim()).match(
+                (error) => setState(() => _messageError = error),
+                (exists) {
+                  if (exists) {
+                    Navigator.pop(context);
+                    Navigator.pushNamed(context, '/addRules', arguments: {
+                      'task_id_1': _youtrackController.text.trim(),
+                      'task_id_2': _jiraController.text.trim(),
+                    });
+                    return;
+                  }
+                  setState(() => _messageError = 'Couldn\'t find a YouTrack issue');
+                },
+              ).run(),
       )
       .run();
 
@@ -338,6 +352,25 @@ class _SelectTasksScreenState extends State<SelectTasksScreen> {
   }
 }
 
+sealed class Relation {
+  const Relation();
+}
+
+class OneToOneRelation extends Relation {
+  const OneToOneRelation();
+}
+
+class ComplexRelation extends Relation {
+  final HashMap<String, String> relatinos;
+  const ComplexRelation(this.relatinos);
+}
+
+class NotInclude extends Relation {
+  const NotInclude();
+}
+
+enum RelationType { oneToOne, complex, notInclude }
+
 class AddRulesScreen extends StatefulWidget {
   final Api api;
   const AddRulesScreen({super.key, required this.api});
@@ -347,24 +380,345 @@ class AddRulesScreen extends StatefulWidget {
 }
 
 class _AddRulesScreenState extends State<AddRulesScreen> {
+  AsyncData<List<String>> _fields = const NoData();
+  List<RelationType>? _ruleTypes;
+  Map<String, Relation> _relations = {};
+  ({String t1, String t2})? _tasksIds;
+
+  @override
+  void initState() {
+    _loadFields();
+    super.initState();
+  }
+
+  Future<void> _loadFields() async {
+    setState(() => _fields = const LoadingData());
+    final fields = await widget.api
+        .getTaskFields()
+        .match<AsyncData<List<String>>>(
+          (error) => ErrorData(error),
+          (d) => SomeData(d),
+        )
+        .run();
+    setState(() {
+      _ruleTypes = switch (fields) {
+        SomeData(:final data) => data.map((e) => RelationType.notInclude).toList(),
+        _ => null,
+      };
+      final relations = switch (fields) {
+        SomeData(:final data) => Map<String, Relation>.fromEntries(
+            data.map(
+              (e) => MapEntry(e, const NotInclude()),
+            ),
+          ),
+        _ => null,
+      };
+      if (relations != null) {
+        _relations = relations;
+      }
+      _fields = fields;
+    });
+  }
+
+  void _setRuleType(int fieldI, String name, RelationType? r) {
+    final inputs = _ruleTypes;
+    if (inputs != null && r != null && fieldI < inputs.length) {
+      setState(() {
+        inputs[fieldI] = r;
+        _relations[name] = switch (r) {
+          RelationType.notInclude => const NotInclude(),
+          RelationType.oneToOne => const OneToOneRelation(),
+          RelationType.complex => ComplexRelation(HashMap()),
+        };
+      });
+      return;
+    }
+  }
+
+  void _addRules() {
+    final tasksIds = _tasksIds;
+    if (tasksIds == null) return;
+    final fields = _relations
+        .map(
+          (key, value) => MapEntry(
+            key,
+            switch (value) {
+              OneToOneRelation _ => {'rule_type': 1},
+              ComplexRelation(:final relatinos) => {'rule_type': 2, 'relations': relatinos},
+              _ => null,
+            },
+          ),
+        )
+        .filter((value) => value != null);
+    final rules = _relations.entries
+        .map(
+          (e) => {
+            'task_id_1': tasksIds.t1,
+            'task_id_2': tasksIds.t2,
+            'fields': fields,
+          },
+        )
+        .toList();
+    widget.api
+        .addRules(rules)
+        .match(
+          (l) => showDialog(
+            context: context,
+            barrierDismissible: true,
+            barrierLabel: 'label',
+            builder: (context) => SizedBox(
+              width: 400,
+              height: 400,
+              child: Material(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Text(l),
+                        const SizedBox(height: 16),
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(Icons.arrow_back),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          (r) => showDialog(
+            context: context,
+            barrierDismissible: true,
+            barrierLabel: 'label',
+            builder: (context) => SizedBox(
+              width: 400,
+              height: 400,
+              child: Material(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Text(
+                          r ? 'Successfully added rules' : 'Couldn\'t add rules',
+                        ),
+                        const SizedBox(height: 16),
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            if (r) {
+                              Navigator.pop(context);
+                            }
+                          },
+                          icon: const Icon(Icons.arrow_back),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+        .run();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
+    _tasksIds = (
+      t1: args['task_id_1'] as String,
+      t2: args['task_id_2'] as String,
+    );
     final textTheme = Theme.of(context).primaryTextTheme;
     return Scaffold(
       body: Center(
         child: SizedBox(
           width: 600,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Enter issues\' IDs',
-                style: textTheme.titleLarge,
+          child: switch (_fields) {
+            ErrorData(:final message) => Text(
+                message,
+                style: textTheme.titleLarge?.copyWith(color: Colors.white54),
               ),
-            ],
-          ),
+            SomeData(:final data) => SingleChildScrollView(
+                padding: const EdgeInsets.symmetric(vertical: 32),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Text('Enter rules', style: textTheme.titleLarge),
+                        const Spacer(),
+                        ElevatedButton(
+                          onPressed: _addRules,
+                          child: const Text('Save'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 32),
+                    ...data.indexed
+                        .map(
+                          (e) {
+                            final index = e.$1;
+                            final selected = switch (_ruleTypes) {
+                              List<RelationType> types => types[index],
+                              null => RelationType.notInclude,
+                            };
+                            return Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(e.$2, style: textTheme.bodyLarge),
+                                    const SizedBox(width: 16),
+                                    Radio<RelationType>(
+                                      value: RelationType.oneToOne,
+                                      groupValue: selected,
+                                      onChanged: (i) => _setRuleType(
+                                        index,
+                                        e.$2,
+                                        i,
+                                      ),
+                                    ),
+                                    const Text('1:1 relation'),
+                                    const SizedBox(width: 16),
+                                    Radio<RelationType>(
+                                      value: RelationType.complex,
+                                      groupValue: selected,
+                                      onChanged: (i) => _setRuleType(
+                                        index,
+                                        e.$2,
+                                        i,
+                                      ),
+                                    ),
+                                    const Text('Match'),
+                                  ],
+                                ),
+                                if (selected == RelationType.complex)
+                                  RelationTypeConstructor(
+                                    relations: _relations,
+                                    relationsKey: e.$2,
+                                  ),
+                              ],
+                            );
+                          },
+                        )
+                        .expand((element) => [const Divider(), element])
+                        .skip(1),
+                  ],
+                ),
+              ),
+            _ => const CircularProgressIndicator(),
+          },
         ),
       ),
+    );
+  }
+}
+
+class RelationTypeConstructor extends StatefulWidget {
+  final Map<String, Relation> relations;
+  final String relationsKey;
+
+  const RelationTypeConstructor({
+    super.key,
+    required this.relations,
+    required this.relationsKey,
+  });
+
+  @override
+  State<RelationTypeConstructor> createState() => _RelationTypeConstructorState();
+}
+
+class _RelationTypeConstructorState extends State<RelationTypeConstructor> {
+  late final TextEditingController _input1;
+  late final TextEditingController _input2;
+
+  @override
+  void initState() {
+    _input1 = TextEditingController();
+    _input2 = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _input1.dispose();
+    _input2.dispose();
+    super.dispose();
+  }
+
+  Relation? get _relations => widget.relations[widget.relationsKey];
+
+  void _remove(String key) => setState(() {
+        switch (_relations) {
+          case ComplexRelation cr:
+            cr.relatinos.remove(key);
+            return;
+          case _:
+            return;
+        }
+      });
+
+  void _save() {
+    final text1 = _input1.text.trim();
+    final text2 = _input2.text.trim();
+    if (text1.isNotEmpty && text2.isNotEmpty) {
+      setState(() {
+        switch (_relations) {
+          case ComplexRelation cr:
+            cr.relatinos[text1] = text2;
+            return;
+          case _:
+            return;
+        }
+      });
+      _input1.clear();
+      _input2.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ...switch (_relations) {
+          ComplexRelation(:final relatinos) => relatinos.entries.map(
+              (e) => Row(
+                children: [
+                  ArrowedTexts([e.key, e.value]),
+                  const SizedBox(width: 8),
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () => _remove(e.key),
+                  ),
+                ],
+              ),
+            ),
+          _ => [],
+        },
+        Row(
+          children: [
+            SizedBox(
+              width: 200,
+              child: TextField(controller: _input1),
+            ),
+            const Icon(Icons.arrow_right_sharp),
+            SizedBox(
+              width: 200,
+              child: TextField(controller: _input2),
+            ),
+            const SizedBox(width: 8),
+            IconButton(
+              icon: const Icon(Icons.save),
+              onPressed: _save,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from threading import Thread
 import synchronizer
 import api_functions
 import json
+from pydantic import BaseModel
+
+class Rules(BaseModel):
+    rules: list
 
 app = FastAPI()
 
@@ -39,21 +43,22 @@ def get_jira_issue(issue_id):
         return False
 
 
-def __add_rule(rule):
-    json_rule = json.loads(rule)
+def __add_rules(rules):
+    new_rules = [json.loads(rule) for rule in rules]
     all_rules = synchronizer.rules
-    task_1 = json_rule["task_id_1"]
-    task_2 = json_rule["task_id_2"]
-    all_rules[f"{task_1}_{task_2}"] = json_rule
+    for r in new_rules:
+        task_1 = r["task_id_1"]
+        task_2 = r["task_id_2"]
+        all_rules[f"{task_1}_{task_2}"] = r
     print(all_rules)
-    with open("../config/config.json", "w") as f:
-        f.write(all_rules)
+    # with open("../config/config.json", "w") as f:
+    #     f.write(all_rules)
 
 
-@app.post("/rule/")
-def add_rule(new_rule):
+@app.post("/rules/")
+def add_rules(new_rules: Rules):
     try:
-        __add_rule(new_rule)
+        __add_rules(new_rules.rules)
         return {"Success": True}
     except Exception:
         return {"Success": False}
